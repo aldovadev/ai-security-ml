82b5941dd59ebd33d1fb9ba2e29e6f93aba099e3
yunomi811 <aldova811@gmail.com>
2023-10-13 20:00:32 +0700

    add(Dockerfile): first deployment

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+#BUILD STAGE USING PYTHON 3.8
+FROM python:3.8
+
+# Set the working directory inside the container
+WORKDIR /
+
+COPY ./ ./
+
+#Upgrade Pip
+RUN pip install --upgrade pip
+
+# Install virtualenv
+RUN pip install virtualenv
+
+# Remove the existing virtual environment if it exists
+RUN rm -rf venv
+
+# Create a virtual environment
+RUN virtualenv venv --python=python3.8
+
+# Activate the virtual environment
+RUN /bin/bash -c "source venv/bin/activate"
+
+# Install cmake and dlib inside the virtual environment
+RUN /bin/bash -c "source venv/bin/activate && pip install cmake dlib"
+
+# Install the Python dependencies from requirements.txt
+RUN /bin/bash -c "source venv/bin/activate && pip install -r requirements.txt"
+
+# Command to run the FastAPI application
+CMD ["/bin/bash", "-c", "source venv/bin/activate && uvicorn main:app --port 8080"]
--- main.py
+++ main.py
@@ -14,12 +14,12 @@ from fastapi.middleware.cors import CORSMiddleware
 import face_recognition
 import starlette
 
+VISITOR_PATH = './db_visitor'
+EMPLOYEE_PATH = './db_employee'
 
-ATTENDANCE_LOG_DIR = './logs'
-DB_PATH = './db'
-for dir_ in [ATTENDANCE_LOG_DIR, DB_PATH]:
-    if not os.path.exists(dir_):
-        os.mkdir(dir_)
+for dir_ in [VISITOR_PATH, EMPLOYEE_PATH]:
+  if not os.path.exists(dir_):
+    os.mkdir(dir_)
 
 app = FastAPI()
 
@@ -33,31 +33,15 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+@app.get("/")
+async def default():
+    # The `return {'status' : 'Server running properly'}` statement is returning a JSON response with
+    # a key-value pair. The key is 'status' and the value is 'Server running properly'. This is a
+    # simple way to indicate that the server is running correctly.
+    return {'status' : 'Server running properly.'}
 
-@app.post("/login")
-async def login(file: UploadFile = File(...)):
-
-    file.filename = f"{uuid.uuid4()}.png"
-    contents = await file.read()
-
-    # example of how you can save the file
-    with open(file.filename, "wb") as f:
-        f.write(contents)
-
-    user_name, match_status = recognize(cv2.imread(file.filename))
-
-    if match_status:
-        epoch_time = time.time()
-        date = time.strftime('%Y%m%d', time.localtime(epoch_time))
-        with open(os.path.join(ATTENDANCE_LOG_DIR, '{}.csv'.format(date)), 'a') as f:
-            f.write('{},{},{}\n'.format(user_name, datetime.datetime.now(), 'IN'))
-            f.close()
-
-    return {'user': user_name, 'match_status': match_status}
-
-
-@app.post("/logout")
-async def logout(file: UploadFile = File(...)):
+@app.post("/visitor/recognize")
+async def recognize_visitor(file: UploadFile = File(...)):
 
     file.filename = f"{uuid.uuid4()}.png"
     contents = await file.read()
@@ -66,20 +50,14 @@ async def logout(file: UploadFile = File(...)):
     with open(file.filename, "wb") as f:
         f.write(contents)
 
-    user_name, match_status = recognize(cv2.imread(file.filename))
+    name, match_status = recognize(cv2.imread(file.filename), VISITOR_PATH)
+    os.remove(file.filename)
 
-    if match_status:
-        epoch_time = time.time()
-        date = time.strftime('%Y%m%d', time.localtime(epoch_time))
-        with open(os.path.join(ATTENDANCE_LOG_DIR, '{}.csv'.format(date)), 'a') as f:
-            f.write('{},{},{}\n'.format(user_name, datetime.datetime.now(), 'OUT'))
-            f.close()
+    return {'visitor_name': name, 'match_status': match_status, 'status' : 200}
+  
+@app.post("/employee/recognize")
+async def recognize_employee(file: UploadFile = File(...)):
 
-    return {'user': user_name, 'match_status': match_status}
-
-
-@app.post("/register_new_user")
-async def register_new_user(file: UploadFile = File(...), text=None):
     file.filename = f"{uuid.uuid4()}.png"
     contents = await file.read()
 
@@ -87,59 +65,87 @@ async def register_new_user(file: UploadFile = File(...), text=None):
     with open(file.filename, "wb") as f:
         f.write(contents)
 
-    shutil.copy(file.filename, os.path.join(DB_PATH, '{}.png'.format(text)))
+    name, match_status = recognize(cv2.imread(file.filename), EMPLOYEE_PATH)
+    os.remove(file.filename)
+
+    return {'employee_name': name, 'match_status': match_status, 'status' : 200}
+  
+
+@app.post("/visitor/add")
+async def add_visitor(file: UploadFile = File(...), name=None):
+    file.filename = f"{uuid.uuid4()}.png"
+    contents = await file.read()
+
+    # # example of how you can save the file
+    with open(file.filename, "wb") as f:
+        f.write(contents)
+
+    shutil.copy(file.filename, os.path.join(VISITOR_PATH, '{}.png'.format(name)))
 
     embeddings = face_recognition.face_encodings(cv2.imread(file.filename))
 
-    file_ = open(os.path.join(DB_PATH, '{}.pickle'.format(text)), 'wb')
+    file_ = open(os.path.join(VISITOR_PATH, '{}.pickle'.format(name)), 'wb')
     pickle.dump(embeddings, file_)
-    print(file.filename, text)
 
     os.remove(file.filename)
 
-    return {'registration_status': 200}
+    return {'message' : 'Add visitor success', 'name' : name, 'status' : 200}
+  
+  
+@app.post("/employee/add")
+async def add_employee(file: UploadFile = File(...), name=None):
+    file.filename = f"{uuid.uuid4()}.png"
+    contents = await file.read()
 
+    # # example of how you can save the file
+    with open(file.filename, "wb") as f:
+        f.write(contents)
 
-@app.get("/get_attendance_logs")
-async def get_attendance_logs():
+    shutil.copy(file.filename, os.path.join(EMPLOYEE_PATH, '{}.png'.format(name)))
 
-    filename = 'out.zip'
+    embeddings = face_recognition.face_encodings(cv2.imread(file.filename))
 
-    shutil.make_archive(filename[:-4], 'zip', ATTENDANCE_LOG_DIR)
+    file_ = open(os.path.join(EMPLOYEE_PATH, '{}.pickle'.format(name)), 'wb')
+    pickle.dump(embeddings, file_)
 
-    ##return File(filename, filename=filename, content_type="application/zip", as_attachment=True)
-    return starlette.responses.FileResponse(filename, media_type='application/zip',filename=filename)
+    os.remove(file.filename)
 
+    return {'message' : 'Add employee success', 'name' : name, 'status' : 200}
+  
+@app.delete("/visitor/reset")
+async def reset_visitor():
+    try:
+        for filename in os.listdir(VISITOR_PATH):
+            file_path = os.path.join(VISITOR_PATH, filename)
+            if os.path.isfile(file_path):
+                os.remove(file_path)
+        return {'message': 'Reset visitor database success', 'status': 200}
+    except Exception as e:
+        return {'message': 'Error resetting visitor database', 'status': 500, 'error': str(e)}
 
-def recognize(img):
+def recognize(img, target):
     # it is assumed there will be at most 1 match in the db
-
     embeddings_unknown = face_recognition.face_encodings(img)
     if len(embeddings_unknown) == 0:
-        return 'no_persons_found', False
+        return 'No person found', False
     else:
         embeddings_unknown = embeddings_unknown[0]
 
     match = False
     j = 0
 
-    db_dir = sorted([j for j in os.listdir(DB_PATH) if j.endswith('.pickle')])
-    # db_dir = sorted(os.listdir(DB_PATH))    
-    print(db_dir)
+    db_dir = sorted([j for j in os.listdir(target) if j.endswith('.pickle')])
+    
     while ((not match) and (j < len(db_dir))):
-
-        path_ = os.path.join(DB_PATH, db_dir[j])
-
+        path_ = os.path.join(target, db_dir[j])
         file = open(path_, 'rb')
         embeddings = pickle.load(file)[0]
-
         match = face_recognition.compare_faces([embeddings], embeddings_unknown)[0]
-
         j += 1
 
     if match:
         return db_dir[j - 1][:-7], True
     else:
-        return 'unknown_person', False
+        return 'Unknown person', False
 
 
